102d4633a8b8b28caf646ecf40e2cec2fe166c6c
Pedro Henrique <pehcs.santos@gmail.com>
2023-11-10 13:27:41 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-
 public class Program {
-
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-
+		
 		while (true) {
 			try {
 				UI.clearScreen();
@@ -22,11 +18,14 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-	
+
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-	
+				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
 			catch (ChessException e) {
